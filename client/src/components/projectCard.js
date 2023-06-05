5eea955f000f2360b691a6524d9a3f058a553eaa
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-06-05 11:11:19 +0700

    Add small components

---
+++ client/src/components/projectCard.js
@@ -0,0 +1,49 @@
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+import { RocketLaunchIcon } from "@heroicons/react/24/solid";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+
+export default function ProjectCard(props) {
+    return (
+        <Card className="mt-6 w-96">
+            <CardBody>
+                <RocketLaunchIcon className="text-blue-500 w-12 h-12 mb-4" />
+                {/* row of 2 compone */}
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                    {props.title}
+                </Typography>
+                <>
+                    <Typography color="blue-gray" className="font-medium">
+                        {new Date(props.due_date).toLocaleDateString()}
+                    </Typography>
+                    <Typography color="blue-gray" className="font-medium">
+                        {() => {
+                            switch (props.status) {
+                                case "in progress":
+                                    return "In Progress";
+                                case "completed":
+                                    return "Completed";
+                            }
+                        }}
+                    </Typography>
+                </>
+                <Typography>
+                    {props.description}
+                </Typography>
+            </CardBody>
+            <CardFooter className="pt-0">
+                <a href="#" className="inline-block">
+                    <Button size="sm" variant="text" className="flex items-center gap-2">
+                        View
+                        <ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
+                    </Button>
+                </a>
+            </CardFooter>
+        </Card>
+    );
+}
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,13 +1,28 @@
 import ProfileCard from "../components/profileCard";
+import axios from "axios";
+import cookie from "cookie";
+import ProjectCard from "../components/projectCard";
+import ReactDOM from "react-dom";
+const getProjects = () => {
+    const cookies = cookie.parse(document.cookie);
+    axios.get("http://localhost:1337/api/projects", { headers: { Authorization: "Bearer " + cookies.token } }).then((res) => {
+        console.log(res.data);
+        const projects = res.data.map((project) => {
+            console.log(project);
+            return (<ProjectCard title={project.title} due_date={project.due_date} status={project.status} description={project.description} />);
+        });
+        ReactDOM.render(<div id="project-cards">{projects}</div>, document.getElementById("root"));
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
 
 const Dashboard = () => {
     return (
         <div className="grid grid-cols-2 gap-5 justify-items-center overflow-auto mt-20">
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <ProjectCard title="Project 1" due_date="2021-10-01" status="in progress" description="This is a description" />
+            {getProjects}
         </div>
     );
 }
