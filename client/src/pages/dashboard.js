ffaf70988eac2410f43a7e35ccd4783ec867a7df
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-06-12 10:51:57 +0700

    update dashboard view

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import cookie from 'cookie';
-import ProjectCard from '../components/projectCard';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Input, Button, Card, CardHeader, CardBody, Typography, CardFooter } from '@material-tailwind/react';
+import '../css/navbar.css';
+import PCWM from '../components/projectCardWithMember';
+import AddTask from '../components/addTaskDialog';
 
 const Dashboard = () => {
     const [projects, setProjects] = useState([]);
@@ -19,20 +23,83 @@ const Dashboard = () => {
                 console.log(err);
             });
     }, []);
+    const handleCreateProject = () => {
+        const cookies = cookie.parse(document.cookie);
+        console.log(cookies.token);
+        axios.post('http://localhost:1337/api/projects', {
 
+            title: 'test1',
+            description: 'test1',
+            due_date: new Date().getTime(),
+            status: 'in progress',
+            team: ['60f9b0b3e0b9c2a8b8f0b0b5']
+
+        }, {
+            headers: { Authorization: `Bearer ${cookies.token}` },
+
+        },)
+            .then(res => {
+                console.log(res.data);
+                setProjects(...res.data);
+            })
+            .catch(err => {
+                alert(err);
+            });
+        // handle create project logic
+    };
     return (
-        <div className="grid grid-cols-2 gap-5 justify-items-center overflow-auto mt-20">
-            {projects.map(project => (
-                <ProjectCard
-                    key={project._id}
-                    id={project._id}
-                    title={project.title}
-                    due_date={project.due_date}
-                    status={project.status}
-                    description={project.description}
-                />
-            ))}
+        <div className="justify-items-center overflow-auto mt-20 ml-20 mr-20">
+            <Card className="h-full w-full">
+                <CardHeader floated={false} shadow={false} className="rounded-none">
+                    <div className="mb-8 flex items-center justify-between gap-8">
+                        <div>
+                            <Typography variant="h2" color="blue-gray">
+                                Projects List
+                            </Typography>
+                            <Typography color="gray" className="mt-1 font-normal">
+                                See information about all project
+                            </Typography>
+                        </div>
+                        <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                            <Button variant="outlined" color="blue-gray" size="sm">
+                                view all
+                            </Button>
+                            <AddTask />
+                        </div>
+                    </div>
+                </CardHeader>
+                <CardBody>
+                    <div className="flex justify-evenly flex-wrap pb-10 gap-10 overflow-auto">
+                        {projects.map(project => (
+                            <PCWM
+
+                                key={project._id}
+                                id={project._id}
+                                title={project.title}
+                                dueDate={project.due_date}
+                                status={project.status}
+                                members={project.members}
+                            />
+                        ))}
+                    </div>
+                </CardBody>
+                <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+                    <Typography variant="small" color="blue-gray" className="font-normal">
+                        Page 1 of 10
+                    </Typography>
+                    <div className="flex gap-2">
+                        <Button variant="outlined" color="blue-gray" size="sm">
+                            Previous
+                        </Button>
+                        <Button variant="outlined" color="blue-gray" size="sm">
+                            Next
+                        </Button>
+                    </div>
+                </CardFooter>
+            </Card>
         </div>
+
+
     );
 };
 
--- client/src/pages/project.js
+++ client/src/pages/project.js
@@ -2,42 +2,87 @@ import { useEffect, useState } from 'react';
 import cookie from 'cookie';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
+import { Card, Chip, CardHeader, CardBody, Typography, Progress } from '@material-tailwind/react';
+import '../css/project.css';
+import TaskTable from '../components/tasksTable';
+import TaskComp from '../components/taskComp';
 
 function Project() {
   const [projectData, setProjectData] = useState(null);
   const [tasks, setTasks] = useState(null);
+  const [members, setMembers] = useState(null);
+
   const id = useParams().id;
   useEffect(() => {
     console.log(id);
     const cookies = cookie.parse(document.cookie);
-    axios.get(`http://localhost:1337/api/projects/${id}`,{headers: { Authorization: `Bearer ${cookies.token}` }})
-      .then(response => setProjectData(response.data))
+    axios.get(`http://localhost:1337/api/projects/${id}`, { headers: { Authorization: `Bearer ${cookies.token}` } })
+      .then(response => {
+        setProjectData(response.data);
+
+      })
       .catch(error => console.error(error));
     axios.get(`http://localhost:1337/api/tasks/project/${id}`, { headers: { Authorization: `Bearer ${cookies.token}` } })
-    .then(res => {
+      .then(res => {
         console.log(res.data);
         setTasks(res.data);
-    })
-    .catch(err => {console.log(err);});
+      })
+      .catch(err => { console.log(err); });
   }, [id]);
 
   //get tasks that in this project
 
-  if (!projectData|| !tasks) {
+  if (!projectData || !tasks) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>{projectData.title}</h1>
-      <p>{projectData.description}</p>
-      {/* Render other project data here */}
-      {tasks.map((task) =>{return (
-            <h1>{task.title}</h1>
-        )}  
-      )}
+    <div className='mt-20 pl-20 pr-20 w-screen justify-items-center overflow-auto'>
+
+      <Card className=' '>
+        <CardBody className='mt-5 pl-5 row justify-between'>
+          <div className='w-4/12'>
+            <Typography className='mb-5' variant="h1" color="blue-gray">{projectData.title}</Typography>
+            {/*increase font */}
+            <Typography color="blue-gray">{projectData.description}</Typography>
+
+          </div>
+          <div className='w-6/12 mt-5 '>
+            <Progress color="lightBlue" value='50' label='Completed' className='h-7 mb-5' />
+
+            <Progress color='red' value='30' label='Lated' className='h-7' />
+          </div>
+          <div className='w-1/12 text-right'>
+            <Chip
+              className='w-max ml-10'
+              variant="ghost"
+              size="sm"
+              value={(projectData.status === 'in progress') ? "In progress" : (projectData.status === "completed") ? "Completed" : "Late"}
+              color={(projectData.status === 'in progress') ? "blue-gray" : (projectData.status === "completed") ? "green" : "red"}
+            />
+            <Typography color="blue-gray">{new Date(projectData.due_date).toISOString().split('T')[0]}</Typography>
+
+          </div>
+        </CardBody>
+      </Card>
+
+      <div className='mt-10 mb-10 row '>
+        <div className='w-3/4 pr-10'>
+
+          <TaskComp />
+
+        </div>
+        <div className='w-1/4'>
+          <Card className=''>
+            <CardBody className='pl-5 flex-col justify-between'>
+              <Typography variant="h5" color="blue-gray">Member</Typography>
+            </CardBody>
+
+          </Card>
+        </div>
+      </div>
     </div>
+
   );
 }
 export default Project;
